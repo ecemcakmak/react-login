7d6ff28b3c9773a500d7de99bedea898684f7e94
ecemcakmak <eng.ecemcakmak@gmail.com>
2022-05-25 14:40:14 +0300

    rev: revize

--- react-login/src/components/App/App.js
+++ react-login/src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
 
@@ -8,16 +8,7 @@ function App() {
   return (
     <div className="wrapper">
       <h1>Application</h1>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+  
     </div>
   );
 }
